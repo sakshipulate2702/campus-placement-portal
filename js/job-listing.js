a37a744cf60c39bdc5cea890003f4ef9b0e90224
sakshipulate2702 <sakshipulate27@gmail.com>
2026-03-07 23:26:19 +0530

    buttons added

--- js/job-listing.js
+++ js/job-listing.js
@@ -1,1 +1,52 @@
-g
+// Search Function
+
+const searchInput = document.getElementById("searchInput");
+const cards = document.querySelectorAll(".card");
+
+searchInput.addEventListener("keyup", function(){
+
+let searchValue = searchInput.value.toLowerCase();
+
+cards.forEach(card => {
+
+let text = card.innerText.toLowerCase();
+
+if(text.includes(searchValue)){
+card.style.display="block";
+}
+else{
+card.style.display="none";
+}
+
+});
+
+});
+
+
+// Filter Function
+
+const filterButtons = document.querySelectorAll(".filter-btn");
+
+filterButtons.forEach(button => {
+
+button.addEventListener("click", () => {
+
+document.querySelector(".active").classList.remove("active");
+button.classList.add("active");
+
+let category = button.dataset.category;
+
+cards.forEach(card => {
+
+if(category === "all" || card.dataset.category === category){
+card.style.display="block";
+}
+else{
+card.style.display="none";
+}
+
+});
+
+});
+
+});
